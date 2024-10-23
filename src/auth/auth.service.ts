85d12a17828da825d6638191b88609fa98495b97
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-10-23 16:29:23 -0300

    Adds jwt to login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,25 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('AuthService');
 
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
+
   onModuleInit() {
     this.$connect();
     this.logger.log('MongoDB Connected');
+  }
+
+  async signJWT(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 
   async registerUser(registerUserDto: RegisterUserDto) {
@@ -31,7 +41,40 @@ export class AuthService extends PrismaClient implements OnModuleInit {
 
       const { password: _, ...rest } = createdUser;
 
-      return { user: rest };
+      return { user: rest, token: await this.signJWT(rest) };
+    } catch (error) {
+      console.log('error.message :', error.message);
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    try {
+      const user = await this.user.findUnique({ where: { email } });
+
+      if (!user) {
+        throw new RpcException({ status: 400, message: 'Invalid credentials' });
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      const { password: __, ...rest } = user;
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
+      };
     } catch (error) {
       console.log('error.message :', error.message);
       throw new RpcException({
